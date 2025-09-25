db0b434e9232eb612f085043b9b3954d5f2098ed
Yogaalditya <www.yogaalditya.com@gmail.com>
2025-09-25 09:30:32 +0800

    create a new card for the date and location section

--- resources/views/frontend/website/components/layouts/banner.blade.php
+++ resources/views/frontend/website/components/layouts/banner.blade.php
@@ -43,45 +43,66 @@
                 @endif
             </div>
 
-            <div class="flex flex-col space-y-4 mb-8">
-                @if($currentScheduledConference->date_start || $currentScheduledConference->date_end)
-                    <div class="flex items-center">
-                        <span class="icon-banner mr-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                            </svg>
-                        </span>
-                        <div>
-                            @if($currentScheduledConference->date_start)
-                                @if($currentScheduledConference->date_end && $currentScheduledConference->date_start->format(Setting::get('format_date')) !== $currentScheduledConference->date_end->format(Setting::get('format_date')))
-                                    <span class="font-semibold color-latest">{{ $currentScheduledConference->date_start->format(Setting::get('format_date')) }}</span>
-                                    <span class="font-semibold color-latest"> - {{ $currentScheduledConference->date_end->format(Setting::get('format_date')) }}</span>
+            @if($bannerUrl)
+                <div class="mt-4 relative">
+                    <img src="{{ $bannerUrl }}" alt="Conference Banner" class="banner-image w-full h-48 md:h-64 object-cover rounded-3xl shadow-md" />
+                    
+                    <!-- Info Card Overlay -->
+                    <div class="banner-info-card absolute -bottom-12 left-1/2 transform -translate-x-1/2 w-full max-w-6xl bg-white rounded-2xl shadow-xl p-6 flex flex-col md:flex-row justify-center items-center gap-8 md:gap-32 border border-gray-100 md:h-[120px]">
+                        
+                        <!-- Location Component -->
+                        <div class="flex flex-col items-start text-left">
+                            <div class="flex items-center gap-2 mb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                                <p class="text-gray-500 text-sm font-medium">Location</p>
+                            </div>
+                            <p class="text-gray-800 font-semibold text-left leading-tight">
+                                {{ new Illuminate\Support\HtmlString($currentScheduledConference->getMeta('location') ?? 'To be announced') }}
+                            </p>
+                        </div>
+
+                        <!-- Start In Component -->
+                        <div class="flex flex-col items-start text-left">
+                            <div class="flex items-center gap-2 mb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <p class="text-gray-500 text-sm font-medium">Start In</p>
+                            </div>
+                            <p class="text-gray-800 font-semibold text-left">
+                                @if($currentScheduledConference->date_start)
+                                    {{ $currentScheduledConference->date_start->format(Setting::get('format_date')) }}
                                 @else
-                                    <span class="font-semibold color-latest">{{ $currentScheduledConference->date_start->format(Setting::get('format_date')) }}</span>
+                                    <span class="text-gray-400">To be announced</span>
                                 @endif
-                            @endif
+                            </p>
+                        </div>
+
+                        <!-- Stop In Component -->
+                        <div class="flex flex-col items-start text-left">
+                            <div class="flex items-center gap-2 mb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                <p class="text-gray-500 text-sm font-medium">Stop In</p>
+                            </div>
+                            <p class="text-gray-800 font-semibold text-left">
+                                @if($currentScheduledConference->date_end)
+                                    {{ $currentScheduledConference->date_end->format(Setting::get('format_date')) }}
+                                @else
+                                    <span class="text-gray-400">To be announced</span>
+                                @endif
+                            </p>
                         </div>
                     </div>
-                @endif
-
-                <div class="flex items-center">
-                    <span class="icon-banner mr-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                        </svg>
-                    </span>
-                    <div>
-                        <span class="font-semibold color-latest">{{ new Illuminate\Support\HtmlString($currentScheduledConference->getMeta('location') ?? 'To be announced') }}</span>
-                    </div>
-                </div>
-            </div>
-
-            @if($bannerUrl)
-                <div class="mt-4">
-                    <img src="{{ $bannerUrl }}" alt="Conference Banner" class="banner-image w-full h-48 md:h-64 object-cover rounded-3xl shadow-md" />
                 </div>
             @endif
         </div>
     </div>
+    
+    <!-- Add margin bottom to account for the overlay card -->
+    <div class="mb-16"></div>
 </section>
